034bf199f503b28e09d46bf4c814ad80fdbf4ecf
dakshsirvi20-wq <dakshsirvi20@gmail.com>
2026-08-14 16:03:57 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4ABiWBvNQKXFOO4PcUyjiFBvho55We4",
+  apiKey: "AIzaSyD4ABiWBvNQdKXFOO4PcUyjiFBvho55We4",
   authDomain: "bhanaram-1308b.firebaseapp.com",
   projectId: "bhanaram-1308b",
   storageBucket: "bhanaram-1308b.firebasestorage.app",
@@ -11,6 +11,8 @@ const firebaseConfig = {
   appId: "1:245762338349:web:7baad83bb807f4008212a4",
   measurementId: "G-5WG8JCVM2Y"
 };
+
+export default firebaseConfig;
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
