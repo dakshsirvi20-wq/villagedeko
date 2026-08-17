3e57930d688dfc60ea1dfe368d393479511f7dc7
dakshsirvi20-wq <dakshsirvi20@gmail.com>
2026-08-17 09:52:07 +0530

    Delete media-config.js

--- media-config.js
+++
@@ -1,4 +0,0 @@
-// Public Cloudinary configuration reused from the previous VillageDeko build.
-// Never put the Cloudinary API Secret in browser code.
-export const CLOUDINARY_CLOUD_NAME = "cyo6vdu5";
-export const CLOUDINARY_UPLOAD_PRESET = "unsigned_preset";
